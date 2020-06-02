a4c6a96dd3ed6be919782c86afac53f4176d2424
Espen Scheuer <espenscheuer@gmail.com>
2020-06-02 13:17:19 -0700

    added react-helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,77 +6,119 @@ import {
   ProfileOutlined,
   LayoutOutlined,
 } from "@ant-design/icons"
-import {  graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Overview from "./overview"
 import Team from "./team"
 import Process from "./process"
 import Deliverable from "./deliverable"
-import Img from "gatsby-image" 
-import "../styles/global.css" // requires a loader 
-
+import Img from "gatsby-image"
+import "../styles/global.css" // requires a loader
+import { Helmet } from "react-helmet"
+import share from "../images/share_logo.png"
 
 function IndexPage({ data }) {
   const { Content, Footer, Sider } = Layout
-  const textStyle = {fontSize : 16, height : 60, display: "flex", alignItems: "center"}
+  const textStyle = {
+    fontSize: 16,
+    height: 60,
+    display: "flex",
+    alignItems: "center",
+  }
   const [page, setPage] = useState("1")
 
   const pages = {
-    "1": <Overview updatePage={setPage} images = {data.overview.edges} />,
+    "1": <Overview updatePage={setPage} images={data.overview.edges} />,
     "2": <Process />,
     "3": <Deliverable />,
-    "4": <Team images = {data.team.edges}/>,
+    "4": <Team images={data.team.edges} />,
   }
 
   return (
-    <Layout>
-      <Sider
-        width = {256}
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={broken => {
-          console.log(broken)
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type)
-        }}
-      >
-        <div className="logo" >
-          {data.overview.edges && <Img fluid={data.overview.edges[2].node.childImageSharp.fluid} alt="" />}
-        </div>
-        <Menu
-          style={{ width: 256 }}
-          theme="dark"
-          mode="inline"
-          selectedKeys={[page]}
-          onClick={e => {
-            setPage(e.key)
+    <>
+      <Helmet htmlAttributes>
+        <html lang="en" />
+        <title>HCDE + Microsoft Teams</title>
+        <meta property="og:image" content={share} />
+        <meta name="description" content="Collaboration between the Human Centered Design and Engineering department at the University of Washington and Microsoft Teams." />
+      </Helmet>
+      <Layout>
+        <Sider
+          width={256}
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken)
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type)
           }}
         >
-          <Menu.Item style = {textStyle} key="1" icon={<ProfileOutlined style = {textStyle} />}>
-            Overview
-          </Menu.Item>
-          <Menu.Item style = {textStyle} key="2" icon={<BlockOutlined style = {textStyle}/>}>
-            Process
-          </Menu.Item>
-          <Menu.Item style = {textStyle} key="3" icon={<LayoutOutlined style = {textStyle} />}>
-            Deliverable
-          </Menu.Item>
-          <Menu.Item style = {textStyle} key="4" icon={<TeamOutlined style = {textStyle} />}>
-            Team
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Content style={{ margin: "24px 16px 0" }}>{pages[page]}</Content>
-        <Footer style={{ textAlign: "center" }}>HCDE + Microsoft Teams</Footer>
+          <div className="logo">
+            {data.overview.edges && (
+              <Img
+                fluid={data.overview.edges[2].node.childImageSharp.fluid}
+                alt=""
+              />
+            )}
+          </div>
+          <Menu
+            style={{ width: 256 }}
+            theme="dark"
+            mode="inline"
+            selectedKeys={[page]}
+            onClick={e => {
+              setPage(e.key)
+            }}
+          >
+            <Menu.Item
+              style={textStyle}
+              key="1"
+              icon={<ProfileOutlined style={textStyle} />}
+            >
+              Overview
+            </Menu.Item>
+            <Menu.Item
+              style={textStyle}
+              key="2"
+              icon={<BlockOutlined style={textStyle} />}
+            >
+              Process
+            </Menu.Item>
+            <Menu.Item
+              style={textStyle}
+              key="3"
+              icon={<LayoutOutlined style={textStyle} />}
+            >
+              Deliverable
+            </Menu.Item>
+            <Menu.Item
+              style={textStyle}
+              key="4"
+              icon={<TeamOutlined style={textStyle} />}
+            >
+              Team
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: "24px 16px 0" }}>{pages[page]}</Content>
+          <Footer style={{ textAlign: "center" }}>
+            HCDE + Microsoft Teams
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   )
 }
 
 export const query = graphql`
   query {
-    team: allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, relativeDirectory: {eq: "team"}}) {
+    team: allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+        relativeDirectory: { eq: "team" }
+      }
+    ) {
       edges {
         node {
           childImageSharp {
@@ -87,7 +129,12 @@ export const query = graphql`
         }
       }
     }
-    overview: allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, relativeDirectory: {eq: "overview"}}) {
+    overview: allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+        relativeDirectory: { eq: "overview" }
+      }
+    ) {
       edges {
         node {
           childImageSharp {
