1eb1de4413c5e61e3b833b894565e71d1f1da1ca
Espen Scheuer <espenscheuer@gmail.com>
2020-06-08 14:47:51 -0700

    iframe

--- src/pages/deliverable.js
+++ src/pages/deliverable.js
@@ -13,14 +13,12 @@ function Deliverable() {
         <Col xs={{ span: 22 }} lg={{ span: 22 }}>
           <Title level={2}>Deliverable</Title>
           <iframe
-            style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
-            width="800"
-            height="450"
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FBr26YD5v8YYqVRLpgo7kLJ%2FFinal-Project%3Fnode-id%3D1%253A8%26scaling%3Dmin-zoom&chrome=DOCUMENTATION"
-            allowfullscreen
-          >
-            fdasfsda
-          </iframe>
+            style={{border: "1px solid rgba(0, 0, 0, 0.1)",
+            width:1100,
+            height:619}}
+            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFZ2uuddJsFhfzNRlsF9BQj%2FCX-App-Prototype%3Fnode-id%3D2%253A0%26scaling%3Dscale-down-width&chrome=DOCUMENTATION"
+            allowFullScreen
+          ></iframe>
         </Col>
         <Col xs={{ span: 1 }} lg={{ span: 1 }} />
       </Row>
